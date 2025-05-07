144bab7ed3d2f6b5c0a1c93f42b7960992014000
Vishwajeet Kumar <vishwajeetkmr7@gmail.com>
2025-05-07 10:52:04 +0530

    Added Router

--- router.js
+++ router.js
@@ -1,2 +1,8 @@
-console.log("I am executed immediately.")
-module.exports = "I am export for the router file"
+const express = require('express')
+const router = express.Router()
+
+const userController = require('./controller/userController')
+
+router.get('/', userController.home)
+
+module.exports = router
