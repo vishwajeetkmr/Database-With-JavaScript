79f06f55f72a631e0896c012cbbd6fa2f2cadad7
Vishwajeet Kumar <vishwajeetkmr7@gmail.com>
2025-05-12 18:17:46 +0530

    Quick Misc. clean up

--- models/User.js
+++ models/User.js
@@ -1,22 +1,40 @@
+const validator = require("validator")
+
 let User = function(data) {
     this.data = data
     this.errors = []
     
 }
 
+User.prototype.clanUp = function() {
+    if (typeof(this.data.username) != "string" ) {this.data.username = ""}
+    if (typeof(this.data.email) != "string" ) {this.data.email = ""}
+    if (typeof(this.data.password) != "string" ) {this.data.password = ""}
+
+    // get rid of any bogus properties
+    this.data = {
+        username: this.data.username.trim().toLowerCase(),
+        email: this.data.email.trim().toLowerCase(),
+        password: this.data.password
+    }
+}
+
 User.prototype.validate = function() {
     if (this.data.username == "") {this.errors.push('You must provide a username')}
-    if (this.data.email == "") {this.errors.push('You must provide a valid email')}
+    if (this.data.username != "" && !validator.isAlphanumeric(this.data.username)) {this.errors.push("Username can only contain letters and numbers.")}
+    if (!validator.isEmail(this.data.email)) {this.errors.push('You must provide a valid email')}
     if (this.data.password == "") {this.errors.push('You must provide a password')}
     if (this.data.password.length > 0 && this.data.password.length <12 ) {this.errors.push("Password must be atleast 12 character long.")}
     if (this.data.password.length > 100) {this.errors.push("Password cannot exceed 100 characters.")}
-    if (this.data.username.length > 0 && this.data.username.length <3) {this.errors.push("Username must be atleast 4 character long.")}
+    if (this.data.username.length > 0 && this.data.username.length <3) {this.errors.push("Username must be atleast 3 character long.")}
     if (this.data.username.length > 30) {this.errors.push("Username cannot exceed 30 characters.")}
 }
 
 
 User.prototype.register = function () {
     // Step #1: Validate user data
+
+    this.clanUp()
     this.validate()
 
     // Step #2: Only if there are no validationerrors
